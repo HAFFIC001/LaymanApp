8401fbbb5581df7b4f801195b4c9d857ee10a3a4
Aryan Gupta <ag980455@gmail.com>
2026-04-01 16:54:58 +0530

    Initial Commit

---
+++ Layman/LaymanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaymanApp.swift
+//  Layman
+//
+//  Created by Aryan Gupta on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LaymanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
